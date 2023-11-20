fe3f0e3768b10e0b2ec8c7d11d94d29fb720ff7d
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-20 09:58:59 +0100

    fix: update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const usdAmountInput = document.getElementById("usdAmount");
   const egpAmountInput = document.getElementById("egpAmount");
 
-  function updateExchangeRate(usdAmount) {
+ function updateExchangeRate(usdAmount) {
     const apiKey = "d73e148bd45c1e9d012fc3a8"; // Replace with your actual API key
     const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/USD/EGP/${usdAmount}`;
 
@@ -10,10 +10,21 @@ document.addEventListener("DOMContentLoaded", function () {
       .then((response) => response.json())
       .then((data) => {
         if (data.result === "success") {
-          const exchangeRateValue = data.conversion_rate;
-          const egpAmount = (usdAmount * exchangeRateValue).toFixed(2);
+          const exchangeRateValue =  data.conversion_rate;
+          const egpAmount = (usdAmount * exchangeRateValue).toFixed(3);
 
           egpAmountInput.value = egpAmount;
+          usdAmountInput.addEventListener("input", function () {
+            const usdAmount = parseFloat(usdAmountInput.value) || 0;
+            updateExchangeRate(usdAmount);
+          });
+
+          egpAmountInput.addEventListener("input", function () {
+            const egpAmount = parseFloat(egpAmountInput.value) || 0;
+
+            const usdAmount = (egpAmount / exchangeRateValue).toFixed(3);
+            usdAmountInput.value = usdAmount;
+          });
         } else {
           console.error("Error in API response:", data["error-type"]);
         }
@@ -23,18 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   }
 
-  usdAmountInput.addEventListener("input", function () {
-    const usdAmount = parseFloat(usdAmountInput.value) || 0;
-    updateExchangeRate(usdAmount);
-  });
-
-  egpAmountInput.addEventListener("input", function () {
-    const egpAmount = parseFloat(egpAmountInput.value) || 0;
-    
-
-    const usdAmount = (egpAmount / exchangeRateValue).toFixed(2);
-    usdAmountInput.value = usdAmount;
-  });
+  
 
   // Initial load
   updateExchangeRate(1); // Default USD amount
